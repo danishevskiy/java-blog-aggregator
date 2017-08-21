ee7c44563c1b6efcac22bc0e79849213bc45fd03
Andrii Danishevskyi <dan1sh@i.ua>
2017-08-21 11:19:13 +0300

    war
    Signed-off-by: Andrii Danishevskyi <dan1sh@i.ua>

--- src/test/java/ua/danish/jba/service/RssServiceTest.java
+++ src/test/java/ua/danish/jba/service/RssServiceTest.java
@@ -24,7 +24,7 @@ public class RssServiceTest {
 	@Test
 	public void testGetItemsFile() throws RssException {
 		List<Item> items = rssService.getItems(new File("test-rss/javavids.xml"));
-		assertEquals(10, items.size());
+		assertEquals(8, items.size());
 		Item firstItem = items.get(0);
 		assertEquals("How to generate web.xml in Eclipse", firstItem.getTitle());
 		assertEquals("23 03 2014 10:01:34", new SimpleDateFormat("dd MM yyyy HH:mm:ss").format(firstItem.getPublishedDate()));
